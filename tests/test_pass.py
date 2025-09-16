90135ee1f1076728bdc9a198aa83065ffb15a3eb
v.ivanova <victoria.ivanova1994@gmail.com>
2025-09-16 17:38:03 +0300

    добавила тесты simple

---
+++ tests/test_pass.py
@@ -0,0 +1,17 @@
+def test_pass10():
+    pass
+
+def test_pass11():
+    pass
+
+def test_pass12():
+    pass
+
+def test_pass13():
+    pass
+
+def test_pass14():
+    pass
+
+def test_pass15():
+    pass
---
+++ tests/test_skipped.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+@pytest.mark.skip
+def test_skipped1():
+    pass
+
+@pytest.mark.skip
+def test_skipped2():
+    pass
+
+@pytest.mark.skip
+def test_skipped3():
+    pass
